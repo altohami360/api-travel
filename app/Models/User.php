8ceda9c9914cc4333215ef16e80213f0f3e9678d
Mohammed Altohami <altohami360@gmail.com>
2023-06-24 14:05:51 +0300

    create schema of application - model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,30 +14,23 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
+    
+    public function roles() 
+    {
+        return $this->belongsToMany(Role::class);
+    }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
